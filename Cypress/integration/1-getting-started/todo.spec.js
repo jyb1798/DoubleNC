a304644d790e6b1ec0c957d12114b3a1b4c1c96b
jyb1798 <bestdung8509@gmail.com>
2022-03-22 10:03:52 +0900

    Add: 타이틀 수정

--- Cypress/integration/1-getting-started/todo.spec.js
+++ Cypress/integration/1-getting-started/todo.spec.js
@@ -1,39 +1,19 @@
 describe("start", () => {
   it("should navigate to the about page", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("https://double-nc.vercel.app/");
 
     cy.get('a[href*="/categories/67"]').click();
 
-    cy.url().should("include", "/categories/62");
+    cy.url().should("include", "/categories/67");
 
     cy.get("p").contains("카페");
 
     cy.get('a[href*="/brands/63"]').click();
 
-    cy.url().should("include", "/brands/173");
-
-    cy.get('a[href*="/items/137"]').click();
-
-    cy.url().should("include", "/items/9996");
-
-    cy.get("div").contains("옵션 선택하기").click();
-
-    cy.get("button").contains("X").click();
+    cy.url().should("include", "/brands/63");
 
     cy.get('img[alt="seeback"]').click();
-
-    cy.get("a").contains("고객센터").click();
-
-    cy.url().should("include", "/contacts");
-
-    cy.get("button").contains("판매").click();
-
-    cy.get('img[alt="seeMore"]').each(($btn, index) => {
-      if (index >= 1) cy.wrap($btn).click();
-    });
-
-    cy.get("button").contains("구매").click();
-
+    
     cy.get('img[alt="seeback"]').click();
   });
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import NavBar from '../Components/Nav/NavBar'
-
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import NavBar from "../Components/Nav/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-        <title>니콘내콘</title>
-        <meta name="description" content="트리온보딩 Team-16 잘하조의 데모사이트입니다." />
-        <meta name="keywords" content="쿠폰,상품권,기프티콘,기프티콘판매,기프티콘구매,기프티콘추천,기프티콘앱,편의점기프티콘,기프티콘선물,기프티콘사용법,모바일쿠폰,모바일상품권,중고쿠폰,중고장터,e쿠폰,할인쿠폰" />
+      <Head>
+        <title>미콘유콘</title>
+        <meta
+          name="description"
+          content="프리온보딩 Team-16 잘하조의 데모사이트입니다."
+        />
+        <meta
+          name="keywords"
+          content="쿠폰,상품권,기프티콘,기프티콘판매,기프티콘구매,기프티콘추천,기프티콘앱,편의점기프티콘,기프티콘선물,기프티콘사용법,모바일쿠폰,모바일상품권,중고쿠폰,중고장터,e쿠폰,할인쿠폰"
+        />
 
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="니콘내콘" />
-        <meta property="og:title" content="니콘내콘 - 국내 1위 기프티콘 플리마켓" />
-        <meta property="og:description" content="모바일 쿠폰 중고거래 사이트. 남들은 정가로 살 때, 나는 할인가로 산다" />
+        <meta property="og:site_name" content="미콘유콘" />
+        <meta
+          property="og:title"
+          content="미콘유콘 - 국내 1위 기프티콘 플리마켓"
+        />
+        <meta
+          property="og:description"
+          content="모바일 쿠폰 중고거래 사이트. 남들은 정가로 살 때, 나는 할인가로 산다"
+        />
         <meta property="og:image" content="./images/ncnc.jpg" />
         <meta property="og:url" content="https://ncnc.app" />
 
         <meta property="twitter:card" content="summary" />
-        <meta property="twitter:site" content="니콘내콘"/>
+        <meta property="twitter:site" content="니콘내콘" />
         <meta property="twitter:title" content="니콘내콘" />
-        <meta property="twitter:description" content="모바일 쿠폰 중고거래 사이트. 남들은 정가로 살 때, 나는 할인가로 산다" />
+        <meta
+          property="twitter:description"
+          content="모바일 쿠폰 중고거래 사이트. 남들은 정가로 살 때, 나는 할인가로 산다"
+        />
         <meta property="twitter:image" content="./images/ncnc.jpg" />
         <meta property="twitter:url" content="https://ncnc.app" />
 
-          <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
 
-    </Head>
-    <Component {...pageProps} />
-  </>
-  )}
-
-export default MyApp
+export default MyApp;
